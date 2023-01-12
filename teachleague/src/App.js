4006dff2bee64e6c9cfb9d2119cfc0c37fbd2542
Eitan Fire <eitanfire@gmail.com>
2023-01-11 23:17:54 -0700

    The App Component

--- teachleague/src/App.js
+++ teachleague/src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Container, Navbar, NavbarBrand } from "reactstrap";
+import vaporwaveTrees from "./app/assets/img/vaporwaveTrees.png";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Navbar dark color="primary" sticky="top" expand="md">
+        <Container>
+          <NavbarBrand href="/">
+            <img src={vaporwaveTrees} alt="Teach League Logo" />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
       Hello World!
     </div>
   );
