11a05727ea54ceac7170dcc87d1fd2e971cb89ab
Eitan Fire <eitanfire@gmail.com>
2023-01-12 22:09:50 -0700

    add semester 1 courses

--- teachleague/src/app/shared/CLASSES.js
+++ teachleague/src/app/shared/CLASSES.js
@@ -2,13 +2,17 @@ import WorldWars from "../assets/img/world-wars-banner.png";
 import Philosophy from "../assets/img/flammarion-engraving.jpg";
 import Policing from "../assets/img/Policing-in-America.png";
 import Finance from "../assets/img/personal-finance.png";
+import Film from "../assets/img/film-banner.png";
+import Epics from "../assets/img/epics-banner.png";
+import Programming from "../assets/img/programming-banner.png";
+import Survival from "../assets/img/survive-the-world-banner.png";
 
 export const CLASSES = [
   {
     id: 0,
     name: "📜 Philosophy",
     image: Philosophy,
-    elevation: 1233,
+    semester: 2,
     featured: true,
     description:
       "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
@@ -17,7 +21,7 @@ export const CLASSES = [
     id: 1,
     name: "🪖 World Wars",
     image: WorldWars,
-    elevation: 877,
+    semester: 2,
     featured: false,
     description:
       "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River.",
@@ -26,7 +30,7 @@ export const CLASSES = [
     id: 2,
     name: "💰 Personal Finance",
     image: Finance,
-    elevation: 2901,
+    semester: 2,
     featured: false,
     description:
       "Let NuCamp be your guide to this off-the-beaten-path, hike-in-only campground.",
@@ -35,7 +39,43 @@ export const CLASSES = [
     id: 3,
     name: "🚔 Policing in America",
     image: Policing,
-    elevation: 42,
+    semester: 2,
+    featured: false,
+    description:
+      "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
+  },
+  {
+    id: 4,
+    name: "📽 Film",
+    image: Film,
+    semester: 2,
+    featured: false,
+    description:
+      "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
+  },
+  {
+    id: 5,
+    name: "👩🏿‍💻 Programming",
+    image: Programming,
+    semester: 1,
+    featured: false,
+    description:
+      "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
+  },
+  {
+    id: 6,
+    name: "⚔ Epics",
+    image: Epics,
+    semester: 2,
+    featured: false,
+    description:
+      "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
+  },
+  {
+    id: 7,
+    name: "🧭 Survive the World!",
+    image: Survival,
+    semester: 2,
     featured: false,
     description:
       "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
