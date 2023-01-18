7b4fa8697ec6ae67162bb980ee129ccd4ffa4a7b
Eitan Fire <eitanfire@gmail.com>
2023-01-17 22:10:41 -0700

    useState() with a random course

--- teachleague/src/pages/CoursesDirectoryPage.js
+++ teachleague/src/pages/CoursesDirectoryPage.js
@@ -2,12 +2,16 @@ import { Container, Row, Col, Button } from "reactstrap";
 import CourseDetail from "../features/courses/CourseDetail";
 import CoursesList from "../features/courses/CoursesList";
 import { selectRandomCourse } from "../features/courses/coursesSlice";
+import { useState } from "react";
 
 const CoursesDirectoryPage = () => {
-    const selectedCourse = selectRandomCourse();
-
-    return (
+    const [selectedCourse, toggleCourse] = useState(selectRandomCourse());
+  return (
+    <>
       <Container>
+        <Button onClick={() => toggleCourse(selectRandomCourse())}>
+          Select Random Course
+        </Button>
         <Row>
           <Col sm="5" md="7">
             <CoursesList />
@@ -17,7 +21,31 @@ const CoursesDirectoryPage = () => {
           </Col>
         </Row>
       </Container>
-    );
+    </>
+  );
 };
+
+// const CoursesDirectoryPage = () => {
+//   let selectedCourse = selectRandomCourse();
+
+//   const toggleCourse = () => {
+//     selectedCourse = selectRandomCourse();
+//     console.log(selectedCourse);
+//   };
+
+//   return (
+//     <Container>
+//       <Button onClick={() => toggleCourse()}>Select Random Course</Button>
+//       <Row>
+//         <Col sm="5" md="7">
+//           <CoursesList />
+//         </Col>
+//         <Col sm="7" md="5">
+//           <CourseDetail campsite={selectedCourse} />
+//         </Col>
+//       </Row>
+//     </Container>
+//   );
+// };
 
 export default CoursesDirectoryPage;
